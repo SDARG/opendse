b59bbad8bf77493f4a32a48eeb8ffedb99c0deac
Felix Reimann <felix@fex-it.de>
2015-11-27 09:14:45 +0100

    javadoc added

--- opendse/opendse-optimization/src/main/java/net/sf/opendse/optimization/io/SpecificationTransformer.java
+++ opendse/opendse-optimization/src/main/java/net/sf/opendse/optimization/io/SpecificationTransformer.java
@@ -2,8 +2,28 @@ package net.sf.opendse.optimization.io;
 
 import net.sf.opendse.model.Specification;
 
+/**
+ * A {@link SpecificationTransformer} changes the {@link Specification} before
+ * the design space exploration starts.
+ * 
+ * @author Felix Reimann
+ *
+ */
 public interface SpecificationTransformer {
-	public int getPriority();
 
+	/**
+	 * Changes the {@link Specification}.
+	 * 
+	 * @param specification
+	 *            the specification to change
+	 */
 	public void transform(Specification specification);
+
+	/**
+	 * The priority changes the order in which {@link SpecificationTransformer}s
+	 * are executed.
+	 * 
+	 * @return the priority
+	 */
+	public int getPriority();
 }
