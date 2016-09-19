fcd71aaa4a20cbd110bb7058db5a303232783d39
Felix Reimann <felix@fex-it.de>
2016-09-19 13:45:44 +0200

    title of viewer added, maximize viewer window

--- opendse/opendse-visualization/src/main/java/net/sf/opendse/visualization/SpecificationViewer.java
+++ opendse/opendse-visualization/src/main/java/net/sf/opendse/visualization/SpecificationViewer.java
@@ -8,8 +8,8 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -61,7 +61,8 @@ public class SpecificationViewer {
 		frame.setLayout(new BorderLayout());
 		frame.add(panel);
 
-		frame.pack();
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		frame.setTitle("OpenDSE Viewer");
 		frame.setVisible(true);
 
 		if (existOnClose) {
