cf78125629106a6214b427e02190163521778f8a
Felix Reimann <felix@fex-it.de>
2017-08-17 21:10:41 +0200

    format javadoc

--- opendse/opendse-optimization/src/main/java/net/sf/opendse/optimization/VariableClassOrder.java
+++ opendse/opendse-optimization/src/main/java/net/sf/opendse/optimization/VariableClassOrder.java
@@ -6,17 +6,17 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 /**
- * The {@link VariableClassOrder} contains a list specifying A) which variables
- * are explored by the genetic algorithm during the design space exploration and
- * B) in which order these variables are set by the SAT solver during the
- * decoding of the SAT genotype. Only variables assignable from classes found in
- * the {@link VariableClassOrder} are used by the genetic algorithm. The
- * assignment of variables which are not assignable from the classes in the
- * {@link VariableClassOrder} (in this case, the indexOf() method returns a -1)
- * is not explored, but instead chosen solely by the SAT-solver. During the
- * SAT-solving, variables that are assignable from classes with a lower index in
- * the {@link VariableClassOrder} are set before variables with a higher index,
- * while variables which are not assignable at all are set at the very end.
+ * The {@link VariableClassOrder} contains variable classes which are explored by the genetic algorithm during the
+ * design space exploration. The order of the variable classes defines in which order the corresponding variables are
+ * set by the SAT solver during the decoding of the SAT genotype.
+ * <p>
+ * Only variables assignable from classes found in the {@link VariableClassOrder} are used by the genetic algorithm. The
+ * assignment of variables which are not assignable from the classes in the {@link VariableClassOrder} (in this case,
+ * the indexOf() method returns {@code -1}) is not explored, but instead chosen solely by the SAT-solver.
+ * <p>
+ * During the SAT-solving, variables that are assignable from classes with a lower index in the
+ * {@link VariableClassOrder} are set before variables with a higher index, while variables which are not assignable at
+ * all are set at the very end.
  * 
  * @author Fedor Smirnov
  *
@@ -24,17 +24,16 @@ import com.google.inject.Singleton;
 @Singleton
 public class VariableClassOrder {
 
-	private final ArrayList<Class<?>> order;
+	private final ArrayList<Class<?>> order = new ArrayList<Class<?>>();
 
 	/**
-	 * In the default case, the object class is the only entry of the
-	 * {@link VariableClassOrder}. Consequently, A) all variables are explored
-	 * by the genetic algorithm and B) no fix order is specified for setting the
-	 * variables by the SAT-solver.
+	 * In the default case, the {@link Object} class is the only entry of the {@link VariableClassOrder}.
+	 * Consequently,<br>
+	 * a) all variables are explored by the genetic algorithm and<br>
+	 * b) no fix order is specified for setting the variables by the SAT-solver.
 	 */
 	@Inject
 	public VariableClassOrder() {
-		order = new ArrayList<Class<?>>();
 		order.add(Object.class);
 	}
 
@@ -48,9 +47,8 @@ public class VariableClassOrder {
 	}
 
 	/**
-	 * Returns the lowest index of a class from which the given object is
-	 * assignable. Returns a -1 if the given object is not assignable from any
-	 * of the classes that can be found in the order list.
+	 * Returns the lowest index of a class from which the given object is assignable. Returns a -1 if the given object
+	 * is not assignable from any of the classes that can be found in the order list.
 	 * 
 	 * @param object
 	 * @return
@@ -65,19 +63,16 @@ public class VariableClassOrder {
 	}
 
 	/**
-	 * Add the given variable class to the variable order. (Optional:) Provide
-	 * this variable with a lower index than the provided variables classes. All
-	 * variable classes that are not assignable from the
-	 * {@link VariableClassOrder} after this operation are ignored, i.e., they
-	 * are not part of the SAT-genotype and are set after all non-ignored
+	 * Add the given variable class to the variable order. (Optional:) Provide this variable with a lower index than the
+	 * provided variables classes. All variable classes that are not assignable from the {@link VariableClassOrder}
+	 * after this operation are ignored, i.e., they are not part of the SAT-genotype and are set after all non-ignored
 	 * variables by the SAT-solver.
 	 * 
 	 * @param variableClass
 	 *            The variable class that is added to the variable order
 	 * @param beforeVariableClasses
-	 *            (optional) The variable classes that should have a higher list
-	 *            index than the variableClass and should be set after
-	 *            variableClass
+	 *            (optional) The variable classes that should have a higher list index than the variableClass and should
+	 *            be set after variableClass
 	 */
 	public void add(Class<?> variableClass, Class<?>... beforeVariableClasses) {
 		// remove the Object.class if it is in the list
