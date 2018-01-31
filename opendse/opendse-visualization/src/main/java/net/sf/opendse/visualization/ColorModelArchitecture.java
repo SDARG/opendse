b32b0dea477dd18b63415026444bec0a68bb88e7
Felix Reimann <felix@fex-it.de>
2018-02-01 00:39:47 +0100

    lvds added

--- opendse/opendse-visualization/src/main/java/net/sf/opendse/visualization/ColorModelArchitecture.java
+++ opendse/opendse-visualization/src/main/java/net/sf/opendse/visualization/ColorModelArchitecture.java
@@ -4,9 +4,9 @@ import java.awt.Color;
 import java.util.HashMap;
 import java.util.Map;
 
-import net.sf.opendse.model.Node;
-
 import com.google.inject.Singleton;
+
+import net.sf.opendse.model.Node;
 
 @Singleton
 public class ColorModelArchitecture implements ColorModel {
@@ -14,10 +14,11 @@ public class ColorModelArchitecture implements ColorModel {
 	Map<String, Color> colors = new HashMap<String, Color>();
 
 	{
-		colors.put("ECU", Graphics.STEELBLUE);
+		colors.put("ECU", Graphics.CHOCOLATE);
 		colors.put("CAN", Graphics.LIGHTSALMON);
 		colors.put("CAN-FD", Graphics.DARKSALMON);
 		colors.put("FlexRay", Graphics.ROSYBROWN);
+		colors.put("LVDS", Graphics.LIGHTGOLDENROD);
 		colors.put("Sensor", Graphics.DODGERBLUE);
 		colors.put("Actuator", Graphics.DODGERBLUE);
 		colors.put("Gateway", Graphics.SADDLEBROWN);
