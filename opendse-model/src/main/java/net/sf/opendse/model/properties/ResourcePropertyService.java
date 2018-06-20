1b27f8ab6c26a69eb106afc4b1f1271a517ef982
Fedor Smirnov <fedor.smirnov@fau.de>
2018-06-20 14:59:24 +0200

    included proxy distance and lower resources in the resource property service

--- opendse-model/src/main/java/net/sf/opendse/model/properties/ResourcePropertyService.java
+++ opendse-model/src/main/java/net/sf/opendse/model/properties/ResourcePropertyService.java
@@ -1,5 +1,8 @@
 package net.sf.opendse.model.properties;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import net.sf.opendse.model.Attributes;
 import net.sf.opendse.model.Resource;
 
@@ -13,7 +16,8 @@ import net.sf.opendse.model.Resource;
 public class ResourcePropertyService extends AbstractPropertyService {
 
 	public enum ResourceAttributes {
-		PROXY_RESOURCE("proxy resource id");
+		PROXY_RESOURCE("proxy resource id"), PROXY_DISTANCE("proxy distance"), LOWER_RESOURCES(
+				"lower proxied resources");
 		protected String xmlName;
 
 		private ResourceAttributes(String xmlName) {
@@ -53,4 +57,86 @@ public class ResourcePropertyService extends AbstractPropertyService {
 		resource.setAttribute(ResourceAttributes.PROXY_RESOURCE.xmlName, proxy.getId());
 	}
 
+	/**
+	 * Returns {@code true} if the given resource has a proxy.
+	 * 
+	 * @param resource
+	 *            The given resource
+	 * @return {@code true} if the given resource has a proxy
+	 */
+	public static boolean hasProxy(Resource resource) {
+		return !getProxyId(resource).equals(resource.getId());
+	}
+
+	/**
+	 * Sets the proxy distance of the given resource. Can only be used for resources
+	 * that have a proxy.
+	 * 
+	 * @param resource
+	 *            the given resource
+	 * @param distance
+	 *            the proxy distance
+	 */
+	public static void setProxyDistance(Resource resource, int distance) {
+		if (!hasProxy(resource)) {
+			throw new IllegalArgumentException("The resource " + resource + " has no proxy.");
+		}
+		String attrName = ResourceAttributes.PROXY_DISTANCE.xmlName;
+		resource.setAttribute(attrName, distance);
+	}
+
+	/**
+	 * Returns the hop distance to the proxy of the given resource. Returns 0 if the
+	 * resource has no proxy.
+	 * 
+	 * @param resource
+	 * @return the hop distance to the proxy of the given resource. Returns 0 if the
+	 *         resource has no proxy.
+	 */
+	public static int getProxyDistance(Resource resource) {
+		String attrName = ResourceAttributes.PROXY_DISTANCE.xmlName;
+		if (!isAttributeSet(resource, attrName)) {
+			return 0;
+		} else {
+			return (Integer) getAttribute(resource, attrName);
+		}
+	}
+
+	/**
+	 * Returns that resources that are below the given resource in the proxy tree.
+	 * Returns an empty set if there are no lower resources.
+	 * 
+	 * @param resource
+	 *            the given resource
+	 * @return the set of resources that are below the given resource in the proxy
+	 *         tree. Returns an empty set if there are no lower resources.
+	 */
+	@SuppressWarnings("unchecked")
+	public static Set<Resource> getLowerResources(Resource resource) {
+		String attrName = ResourceAttributes.LOWER_RESOURCES.xmlName;
+		if (!isAttributeSet(resource, attrName)) {
+			return new HashSet<Resource>();
+		} else {
+			return (HashSet<Resource>) getAttribute(resource, attrName);
+		}
+	}
+
+	/**
+	 * Add the given lowerResource to the lower Resources annotated for the given
+	 * resource.
+	 * 
+	 * @param resource
+	 *            the given resource (on a higher level in the proxy tree)
+	 * @param lowerResource
+	 *            (under the resource in the proxy tree)
+	 */
+	public static void addLowerResource(Resource resource, Resource lowerResource) {
+		if (!hasProxy(lowerResource)) {
+			throw new IllegalArgumentException("The resource " + lowerResource + " has no proxy.");
+		}
+		String attrName = ResourceAttributes.LOWER_RESOURCES.xmlName;
+		Set<Resource> lowerResources = getLowerResources(resource);
+		lowerResources.add(lowerResource);
+		resource.setAttribute(attrName, lowerResources);
+	}
 }
--- opendse-model/src/test/java/net/sf/opendse/model/properties/ResourcePropertyServiceTest.java
+++ opendse-model/src/test/java/net/sf/opendse/model/properties/ResourcePropertyServiceTest.java
@@ -2,6 +2,8 @@ package net.sf.opendse.model.properties;
 
 import static org.junit.Assert.*;
 
+import java.util.Set;
+
 import org.junit.Test;
 
 import net.sf.opendse.model.Resource;
@@ -13,10 +15,34 @@ public class ResourcePropertyServiceTest {
 		String idStringRes = "res";
 		Resource res = new Resource(idStringRes);
 		assertEquals(idStringRes, ResourcePropertyService.getProxyId(res));
+		assertFalse(ResourcePropertyService.hasProxy(res));
+		Set<Resource> lower = ResourcePropertyService.getLowerResources(res);
+		assertTrue(lower.isEmpty());
+		assertEquals(0, ResourcePropertyService.getProxyDistance(res));
 		String proxyId = "proxy";
 		Resource proxy = new Resource(proxyId);
 		ResourcePropertyService.setProxyId(res, proxy);
 		assertEquals(proxyId, ResourcePropertyService.getProxyId(res));
+		assertTrue(ResourcePropertyService.hasProxy(res));
+		ResourcePropertyService.setProxyDistance(res, 1);
+		assertEquals(1, ResourcePropertyService.getProxyDistance(res));
+		lower.add(res);
+		ResourcePropertyService.addLowerResource(proxy, res);
+		assertTrue(ResourcePropertyService.getLowerResources(proxy).size() == 1);
+		assertTrue(ResourcePropertyService.getLowerResources(proxy).contains(res));
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testExceptionLower() {
+		Resource res = new Resource("res");
+		Resource res1 = new Resource("res1");
+		ResourcePropertyService.addLowerResource(res, res1);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testException() {
+		Resource res = new Resource("res");
+		ResourcePropertyService.setProxyDistance(res,1);
 	}
 
 }
